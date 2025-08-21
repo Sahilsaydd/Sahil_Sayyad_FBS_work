efe9922a2c9bf96ce6469394b3a06a6ce85fd1a9
Sahil sayyad <sayyadsahil124@gmail.com>
2025-08-21 16:46:07 +0530

    Delete Geometry_Program directory

--- Geometry_Program/Area_Of_Rectangle.py
+++
@@ -1,6 +0,0 @@
-length = float(input("Enter the Length of the Rectangle :"))
-width = float(input("Enter the Width of the Rectangle :"))
-area = length * width
-print("Area of Rectangle is :", area)
-# This program calculates the area of a rectangle given its length and width.
-# The formula used is: Area = Length * Width    
--- Geometry_Program/Perimeter_of_rectangle.py
+++
@@ -1,4 +0,0 @@
-length =float(input("Enter the length of the rectangle:"))
-width = float(input("Enter the width of the rectangle"))
-perimeter = 2*(length+width)
-print("Perimeter of Rectangle is :",perimeter)
