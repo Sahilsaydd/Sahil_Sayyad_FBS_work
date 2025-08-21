3144dc383ed829f131780abe972cfcff798ce97c
Sahil sayyad <sayyadsahil124@gmail.com>
2025-08-21 16:44:30 +0530

    Delete Assignment_2 directory

--- Assignment_2/Fahrenheit_Calculate.py
+++
@@ -1,3 +0,0 @@
-c=30
-Fahrenheit = (c*9/5)+32
-print("The Total Fahrenheit is :",Fahrenheit)
--- Assignment_2/Swap_Number_without_variable.py
+++
@@ -1,9 +0,0 @@
-a =int(input("Enter the first number :"))
-b=int(input("Enter the second Number:"))
-
-print(f"Before Swapping: a= {a} b ={b}")
-a=a+b
-b=a-b
-a=a-b
-
-print(f"After Swapping: a= {a} b= {b}")
