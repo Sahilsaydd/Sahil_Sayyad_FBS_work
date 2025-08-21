351510b00022483ee64528e4d35ea50d02b90d96
Sahil sayyad <sayyadsahil124@gmail.com>
2025-08-21 16:40:33 +0530

    Delete Basic_Program directory

--- Basic_Program/add.py
+++
@@ -1,6 +0,0 @@
-num1= 10
-num2= 20
-sum= num1+num2
-print("Addition is :",sum)
-print("Addition is "+str(sum))
-print(f'The sum of  {num1} and {num2} is {sum}') # Using f-string for formatted output
--- Basic_Program/input_function.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Enter the number  1 "))
-num2 = int(input("Enter the number 2"))
-
-sum = num1+num2
-print("addition is :",sum)
-print("Addition is "+str(sum))
-print(f'The sum of {num1} and {num2} is {sum}')
